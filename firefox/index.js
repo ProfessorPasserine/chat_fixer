dfe37b7b864a454929aca3ffe53953855c54ff12
Professor Passerine <ProfessorPasserine@gmail.com>
2016-02-01 01:15:13 -0800

    Adjusted site matching

--- firefox/index.js
+++ firefox/index.js
@@ -2,6 +2,6 @@ var pageMod = require("sdk/page-mod"),
     self = require("sdk/self");
 
 pageMod.PageMod({
-  include: "https://www.sofurry.com/chat",
+  include: "*sofurry.com/chat",
   contentScriptFile: [self.data.url("cache_buster.js"), self.data.url("highlight_override.js")]
 })
